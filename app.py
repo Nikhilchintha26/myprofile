79fe6563f23ab46a36ea2c40722feda382323397
Nikhilchintha26 <124909308+Nikhilchintha26@users.noreply.github.com>
2025-01-23 11:19:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/download-resume', methods=['GET'])
 def download_resume():
-    resume_path = r"C:\Users\saich\OneDrive\Desktop\vscode\html\myprofile\resume.pdf"  # Replace with the actual path to your resume file
+    resume_path = r"resume.pdf"
     return send_file(resume_path, as_attachment=True)
 
 if __name__ == '__main__':
